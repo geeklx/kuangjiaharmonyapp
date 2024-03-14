36674a5dd736d89943ec37734577a00d633c08ba
geekharmony <liangxiao6@live.com>
2024-03-14 16:06:33 +0800

    meger新增欢迎页

--- product/phone1/src/main/ets/phone1ability/Phone1Ability.ts
+++ product/phone1/src/main/ets/phone1ability/Phone1Ability.ts
@@ -14,8 +14,21 @@ export default class Phone1Ability extends UIAbility {
   onWindowStageCreate(windowStage: window.WindowStage) {
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
-
-    windowStage.loadContent('pages/LaunchPage', (err, data) => {
+    // 获取窗口实例并赋值给全局变量，如此所有页面都可以通过全局变量去修改窗口信息，不需要重新获取
+    // windowStage.getMainWindow((err, data) => {
+    //   if (err.code) {
+    //     console.error('获取失败' + JSON.stringify(err));
+    //     return;
+    //   }
+    //   console.info('获取主窗口的实例：' + JSON.stringify(data));
+    //   globalThis.windowClass = data // 赋值给全局变量windowClass
+    //   // 设置全屏显示
+    //   globalThis.windowClass.setWindowLayoutFullScreen(true)
+    //   // 隐藏状态栏显示
+    //   globalThis.windowClass.setWindowSystemBarEnable(['navigation'])
+    // });
+    //
+    windowStage.loadContent('pages/LauncherPage', (err, data) => {
       if (err.code) {
         hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
         return;
